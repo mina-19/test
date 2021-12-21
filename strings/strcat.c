41b0469fc8a4a28213b31a234040b22a8d4b0503
meenu <meenur200697@gmail.com>
2021-12-21 13:00:22 +0530

    strcat

---
+++ strings/strcat.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+char *mystrcat(char *dst,char *src){
+
+int i,j=0;
+for(i=strlen(dst);src[j];i++){
+	dst[i]=src[j];
+	j++;
+
+}
+dst[i]='\0';
+return dst;
+
+}
+void main(){
+char s1[50],s2[10],*p;
+scanf("%s",s1);
+scanf("%s",s2);
+p=mystrcat(s1,s2);
+printf("%s\n",p);
+}
